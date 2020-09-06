b14d8a625bd8cab71c36aebc8ccc102bea2de3e4
nihal <nihalpereira1998@gmail.com>
2020-09-06 20:04:13 +0530

    split into classes

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,20 +1,21 @@
 package vitals;
 
 public class Main {
-    static boolean vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
-        assert(vitalsAreOk(80, 97, 40) == true);
-        assert(vitalsAreOk(60, 98, 40) == false);
-        System.out.println("Some more tests needed");
+    	VitalsLogic.addVital(new Vital("bpm", 70, 150, 1600));
+    	VitalsLogic.addVital(new Vital("spo2", 90, 200, 97));
+    	VitalsLogic.addVital(new Vital("water", 30, 150, 40));
+        //assert(vitalsAreOk(80, 97, 40) == true);
+        //assert(vitalsAreOk(60, 98, 40) == false);
+    	
+        VitalsLogic.check();
+        
+        VitalsLogic.addVital(new Vital("bpm", 70, 150, 130));
+    	VitalsLogic.addVital(new Vital("spo2", 90, 200, 97));
+    	VitalsLogic.addVital(new Vital("water", 30, 150, 40));
+    	
+    	VitalsLogic.check();
+    	
     }
 }
