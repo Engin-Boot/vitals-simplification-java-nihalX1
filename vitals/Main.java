33c260ada12203a4f38e93c73af785468f3046de
nihal <nihalpereira1998@gmail.com>
2020-09-06 20:59:08 +0530

    seperated tests

--- vitals/Main.java
+++ vitals/Main.java
@@ -3,19 +3,7 @@ package vitals;
 public class Main {
 
     public static void main(String[] args) {
-    	VitalsLogic.addVital(new Vital("bpm", 70, 150, 1600));
-    	VitalsLogic.addVital(new Vital("spo2", 90, 200, 97));
-    	VitalsLogic.addVital(new Vital("water", 30, 150, 40));
-        //assert(vitalsAreOk(80, 97, 40) == true);
-        //assert(vitalsAreOk(60, 98, 40) == false);
-    	
-        VitalsLogic.check();
-        
-        VitalsLogic.addVital(new Vital("bpm", 70, 150, 130));
-    	VitalsLogic.addVital(new Vital("spo2", 90, 200, 97));
-    	VitalsLogic.addVital(new Vital("water", 30, 150, 40));
-    	
-    	VitalsLogic.check();
+    	Tests.runTests();
     	
     }
 }
